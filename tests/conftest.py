24123d2c75cf6da43898c1a65805b8ef50cb4aee
Edward Amor <edward.amor3@gmail.com>
2022-01-05 09:13:41 +0000

    fix: add global conftest

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+def pytest_sessionfinish(session, exitstatus):
+    if exitstatus == pytest.ExitCode.NO_TESTS_COLLECTED:
+        # we treat "no tests collected" as passing
+        session.exitstatus = pytest.ExitCode.OK
+
+
+@pytest.fixture(scope="module", autouse=True)
+def mod_isolation(chain):
+    chain.snapshot()
+    yield
+    chain.revert()
+
+
+@pytest.fixture(autouse=True)
+def isolation(chain, history):
+    start = len(history)
+    yield
+    end = len(history)
+    if end - start > 0:
+        chain.undo(end - start)
